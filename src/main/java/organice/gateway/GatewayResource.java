b2e601f52259168ee0b1bfaee7ed15df378eb669
PedroPertusi <pedrovazpertusi@gmail.com>
2024-03-12 15:11:54 -0300

    gateway initializing

---
+++ src/main/java/organice/gateway/GatewayResource.java
@@ -0,0 +1,14 @@
+package organice.gateway;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GatewayResource {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Gateway!";
+    }
+
+}
