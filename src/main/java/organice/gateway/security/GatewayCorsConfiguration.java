f09acf1c90eb2b87665d395e0958f9b5ea2fbdbe
alfredjynx <99195331+alfredjynx@users.noreply.github.com>
2024-05-29 07:44:52 -0300

    peryusi

---
+++ src/main/java/organice/gateway/security/GatewayCorsConfiguration.java
@@ -0,0 +1,32 @@
+
+package organice.gateway.security;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class GatewayCorsConfiguration {
+
+    private static final Logger logger = LoggerFactory.getLogger(GatewayCorsConfiguration.class);
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowCredentials(false);
+        corsConfig.addAllowedOrigin("*");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        logger.info("CORS configuration applied: " + corsConfig.toString());
+
+        return new CorsWebFilter(source);
+    }
+}
